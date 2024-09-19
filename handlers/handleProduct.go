7ca8c51f9be6a500ede493540c2886dcbca30609
KlassnayaAfrodita <113383200+KlassnayaAfrodita@users.noreply.github.com>
2024-09-19 23:40:46 +0300

    Add files via upload

---
+++ handlers/handleProduct.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (api *Api) GetAllProduct(w http.ResponseWriter, r *http.Request) {
+	products, err := api.productStorage.GetProducts()
+	if err != nil {
+		http.Error(w, `{"error":"db error"}`, 500)
+		return
+	}
+
+	resp, err := json.Marshal(products)
+	if err != nil {
+		http.Error(w, `{"error":"json error"}`, 500)
+	}
+
+	w.Write(resp)
+}

679a0032e2ae56660daee8ad399bae3c3f76f666
KlassnayaAfrodita <evgeny.kazanjan@yandex.ru>
2024-09-20 00:13:16 +0300

    update

--- handlers/handleProduct.go
+++ handlers/handleProduct.go
@@ -3,6 +3,9 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func (api *Api) GetAllProduct(w http.ResponseWriter, r *http.Request) {
@@ -20,6 +23,24 @@ func (api *Api) GetAllProduct(w http.ResponseWriter, r *http.Request) {
 	w.Write(resp)
 }
 
-func (api *Api) GetProduct(w http.ResponseWriter, r *http.Request) {
+func (api *Api) GetProduct(w http.ResponseWriter, r *http.Request) { //! у нас есть id
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, `{"error":"bad id"}`, 400)
+		return
+	}
 
+	product, err := api.productStorage.GetProduct(id)
+	if err != nil {
+		http.Error(w, `{"error":"db error"}`, 500)
+		return
+	}
+
+	resp, err := json.Marshal(product)
+	if err != err {
+		http.Error(w, `{"error":"json error"}`, 500)
+	}
+
+	w.Write(resp)
 }
